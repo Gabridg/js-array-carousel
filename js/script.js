b29abb5f89112ad4c4c727ddc59ff8a5e8220887
Gabridg <gabri99.del@gmail.com>
2022-06-28 12:09:43 +0200

    Task COMPLETE

--- js/script.js
+++ js/script.js
@@ -39,13 +39,56 @@ Abbiamo completato ormai da qualche settimana la sessione HTML e CSS, se non
 ci ricordiamo qualcosa andiamo pure a riguardare alcuni argomenti. Non 
 dedichiamo però al ripasso più di una mezz'ora, così da non perdere di vista 
 il focus dell'esercizio.
-Consigli del giorno:
-1. Costruiamo del carosello una versione statica contenente un'immagine grande 
-con del testo ben posizionato e una miniatura. Di questa versione statica al 
-momento opportuno commenteremo (oscureremo) alcuni elementi per poterli riprodurre 
-dinamicamente in js. Potremo quindi usarli come "template".
-2. Scriviamo sempre prima per punti il nostro algoritmo in italiano per capire 
-cosa vogliamo fare
-3. Al momento giusto (starà a voi capire quale) rispondete a questa domanda: 
-"Quanti cicli servono?"
 */
+
+
+
+
+// MILESTONE 2
+const source = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg", "img/06.jpg", "img/07.jpg", "img/08.jpg", "img/09.jpg", "img/10.jpg"];
+
+const carousel = document.getElementById('carousel');
+const nextButton = document.getElementById('next');
+const backButton = document.getElementById('back');
+
+let imageElement = '';
+
+for (let i = 0; i < source.length; i++) {
+    imageElement += `<img src=${source[i]} alt="landscape ${i}" />`;
+}
+
+carousel.innerHTML = imageElement;
+
+const images = document.querySelectorAll('#carousel img');
+
+
+let currentActiveIndex = 0;
+
+images[currentActiveIndex].classList.add('active');
+
+
+// MILESTONE 3 + Bonus 1
+nextButton.addEventListener('click', function () {
+    images[currentActiveIndex].classList.remove('active');
+
+    currentActiveIndex++;
+
+    if (currentActiveIndex === images.length) {
+        currentActiveIndex = 0;
+    }
+
+    images[currentActiveIndex].classList.add('active');
+})
+
+
+backButton.addEventListener('click', function () {
+    images[currentActiveIndex].classList.remove('active');
+
+    currentActiveIndex--;
+
+    if (currentActiveIndex < 0) {
+        currentActiveIndex = images.length - 1;
+    }
+
+    images[currentActiveIndex].classList.add('active');
+})
